b930df90bae294778d79863e3296d4f757d3b119
Dennis Mücklich <58838393+dmueckli@users.noreply.github.com>
2021-06-07 09:27:43 +0200

    Update AD-Checker.bat

--- AD-Checker.bat
+++ AD-Checker.bat
@@ -2,45 +2,55 @@
 :: Veröffenticht unter GNU GPL v3
 :: Dieses Script prüft ob ein Benutzer einer Gruppe im Active Directory zugewiesen ist. (quick&dirty)
 @echo off
-echo ##################################################################
+echo ######################################################################
 echo.
-echo ___  ____   _      _        
-echo |  \/  (_) (_)    | |       
-echo | .  . |_   _  ___| | _____ 
-echo | |\/| | | | |/ __| |/ / _ \
-echo | |  | | |_| | (__|   <  __/
-echo \_|  |_/\__,_|\___|_|\_\___|
+echo                        ___  ____   _      _        
+echo                        |  \/  (_) (_)    | |       
+echo                        | .  . |_   _  ___| | _____ 
+echo                        | |\/| | | | |/ __| |/ / _ \
+echo                        | |  | | |_| | (__|   <  __/
+echo                        \_|  |_/\__,_|\___|_|\_\___|
 echo                             
 echo                             
 echo.
-echo ##################################################################
+echo ######################################################################
+echo                           Service Desk
 echo.
-echo                      Benutzergruppenpruefer
+echo                      ActiveDirectory Checker
+echo ######################################################################
 echo.
-echo ##################################################################
-echo.
-
-:: Copyright Dennis Muecklich 2021 
-:: Veröffenticht unter GNU GPL v3
-:: Dieses Script prüft ob ein Benutzer einer Gruppe im Active Directory zugewiesen ist. (quick&dirty)
-@echo off
 
 :Start
 set benutzername=""
 set gruppe=""
 set checker=""
-set funktion== 0
+set funktion==0
 
-set /p funktion=Prüfen ob Benutzer einer Gruppe zugeordnet ist(0, Enter), Gruppensuche(1), Benutzersuche(2) ?
+set /p funktion=Gruppenzuordnung(0, Enter), Gruppensuche(1), Benutzersuche(2), Kontostatus(3) ?
+
+echo.
+echo ######################################################################
+echo.
+
 if %funktion%==0 goto Benutzergruppenpruefer
 if %funktion%==1 goto Gruppensuche
 if %funktion%==2 goto Benutzersuche
+if %funktion%==3 goto Kontostatus
 
 :Benutzergruppenpruefer
+VERIFY > nul
+cmd /c "exit /b 0"
+ver > nul
+
+echo Gruppenzuordnung
+echo.
+
+:Benutzer
+
 set /p benutzername="Bitte den Benutzernamen eingeben: "
 echo.
-if %benutzername%=="" echo Der Benutzername darf nicht leer sein. && echo. && goto Benutzergruppenpruefer
-net user %benutzername% /domain 2>NUL | findstr %benutzername% && echo. || echo Der Benutzer konnte nicht gefunden werden! && echo. && echo ################################################################## && echo. && goto Start
+if %benutzername%=="" echo Der Benutzername darf nicht leer sein! && echo. && echo ###################################################################### && echo. && goto Benutzer
+net user %benutzername% /domain 2>NUL | findstr %benutzername% && echo. || echo Der Benutzer konnte nicht gefunden werden! && echo. && echo ###################################################################### && echo. && goto Start
 
 :Gruppe
 VERIFY > nul
@@ -49,16 +59,18 @@ ver > nul
 set /p gruppe="Bitte die Gruppe eingeben: "
 echo.
 if %gruppe%=="" echo Die Eingabe darf nicht leer sein. && echo. && goto Gruppe
-net group /domain 2>NUL | findstr /x *%gruppe% && echo. || echo Die Gruppe %gruppe% konnte nicht gefunden werden! && echo. && echo ################################################################## && echo. && goto Start
+net group /domain 2>NUL | findstr /x *%gruppe% && echo. || echo Die Gruppe %gruppe% konnte nicht gefunden werden! && echo. && echo ###################################################################### && echo. && goto Start
 
-echo ##################################################################
+net group %gruppe% /domain | findstr "Beschreibung 
+echo.
+echo ######################################################################
 echo.
 
 for /f %%i in ('net user %benutzername% /domain ^| findstr *%gruppe%') do set checker=%%i
 
 if %checker%==*%gruppe% ( echo Der Benutzer %benutzername% ist der Gruppe %gruppe% zugewiesen. ) else ( echo Der Benutzer %benutzername% ist NICHT der Gruppe %gruppe% zugewiesen! )
 echo.
-echo ##################################################################
+echo ######################################################################
 echo.
 
 goto Start
@@ -68,24 +80,59 @@ VERIFY > nul
 cmd /c "exit /b 0"
 ver > nul
 
-set /p gruppe="Bitte die Gruppe eingeben: "
+echo Gruppensuche
+echo.
+
+set /p gruppe="Bitte den Suchbegriff eingeben: "
 echo.
 if %gruppe%=="" echo Die Eingabe darf nicht leer sein. && echo. && goto Gruppensuche
-net group /domain 2>NUL | findstr *%gruppe% && echo. || echo Die Gruppe %gruppe% konnte nicht gefunden werden! && echo. && echo ################################################################## && echo. && goto Start
+echo Suchergebnis:
+net group /domain 2>NUL | findstr /I %gruppe% && echo. || echo Die Gruppe %gruppe% konnte nicht gefunden werden! && echo. && echo ###################################################################### && echo. && goto Start
 
-echo ##################################################################
+echo ######################################################################
 echo.
+
+goto Start
 
 :Benutzersuche
 VERIFY > nul
 cmd /c "exit /b 0"
 ver > nul
 
+echo Benutzersuche
+echo.
+
 set /p benutzername="Bitte den Suchbegriff eingeben: "
 echo.
 if %benutzername%=="" echo Der Suchbegriff darf nicht leer sein. && echo. && goto Benutzersuche
-net user /domain 2>NUL | findstr %benutzername% && echo. || echo Der Benutzer konnte nicht gefunden werden! && echo. && echo ################################################################## && echo. && goto Start
+echo Suchergebnis:
+net user /domain 2>NUL | findstr /I %benutzername% && echo. || echo Der Benutzer konnte nicht gefunden werden! && echo. && echo ###################################################################### && echo. && goto Start
+
+echo ######################################################################
+echo.
+
+goto Start
+
+:Kontostatus
+VERIFY > nul
+cmd /c "exit /b 0"
+ver > nul
+
+echo Kontostatus
+echo.
+
+set /p benutzername="Bitte den Benutzernamen eingeben: "
+echo.
+if %benutzername%=="" echo Der Benutzername darf nicht leer sein. && echo. && goto Kontostatus
+echo Suchergebnis:
+net user %benutzername% /domain 2>NUL | findstr /I "Benutzername aktiv letzte Vollst" && echo. || echo Der Benutzer konnte nicht gefunden werden! && echo. && echo ###################################################################### && echo. && goto Start
+
+echo ######################################################################
+echo.
 
 goto Start
 
 :Exit
+
+:: Don't worry if it doesn't work right. If everything did, you'd be out of job.
+:: - Mosher's law of software engineering
